3a6f67e79b2828017ba37aafdce19485490397ec
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2026-08-16 13:26:32 +0300

    Миграции

---
+++ src/Mnemora.Infrastructure/Migrations/20260816102041_AddMaterials.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mnemora.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaterials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "materials",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    topic_id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    title = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    difficulty = table.Column<int>(type: "INTEGER", nullable: false),
+                    icon = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    study_points = table.Column<int>(type: "INTEGER", nullable: false),
+                    review_points = table.Column<int>(type: "INTEGER", nullable: false),
+                    learning_revision = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    type = table.Column<int>(type: "INTEGER", nullable: false),
+                    article_id = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_materials", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_materials_materials_article_id",
+                        column: x => x.article_id,
+                        principalTable: "materials",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_materials_topics_topic_id",
+                        column: x => x.topic_id,
+                        principalTable: "topics",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "material_tags",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    value = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    material_id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_material_tags", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_material_tags_materials_material_id",
+                        column: x => x.material_id,
+                        principalTable: "materials",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_material_tags_material_id_value",
+                table: "material_tags",
+                columns: new[] { "material_id", "value" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_materials_article_id",
+                table: "materials",
+                column: "article_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_materials_topic_id",
+                table: "materials",
+                column: "topic_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "material_tags");
+
+            migrationBuilder.DropTable(
+                name: "materials");
+        }
+    }
+}
