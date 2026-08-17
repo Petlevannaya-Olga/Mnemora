c8a661e231fdf59e4bdcbf19ae2a1ac2971bb4ea
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2026-08-17 18:45:13 +0300

    Второй шаг создания материала

--- src/Mnemora.Desktop/Views/Library/CreateMaterialView.xaml.cs
+++ src/Mnemora.Desktop/Views/Library/CreateMaterialView.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Mnemora.Desktop.Views.Library;
@@ -7,5 +8,17 @@ public partial class CreateMaterialView : UserControl
     public CreateMaterialView()
     {
         InitializeComponent();
+    }
+
+    // Только локальная навигация между макетами шагов.
+    // Создание материала и Application-команды здесь не подключены.
+    private void GoToBasicStep_OnClick(object sender, RoutedEventArgs e)
+    {
+        WizardTabs.SelectedIndex = 1;
+    }
+
+    private void GoToTypeStep_OnClick(object sender, RoutedEventArgs e)
+    {
+        WizardTabs.SelectedIndex = 0;
     }
 }
